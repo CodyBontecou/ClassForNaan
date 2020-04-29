d2942bdd6882b8e858b5036aec0b345f4c2596ef
Cody Bontecou <bontecouc@gmail.com>
2020-04-29 09:14:12 -0500

    using dictionary.items() to iterate through a dicitonary

--- Homework_2_nested_for_loop.py
+++ Homework_2_nested_for_loop.py
@@ -10,19 +10,47 @@
 
 data = [
     {
-       1: {"name": "Andrea", "age": "29", "nationality": "mexican"}
+        1: {"name": "Andrea", "age": "29", "nationality": "mexican"}
     },
     {
-       1: {"name": "Cody", "age": "26", "nationality": "hawaiian"}
+        2: {"name": "Cody", "age": "26", "nationality": "hawaiian"}
 
     },
     {
-       1: {"name": "Daniela", "age": "23", "nationality": "mexican"}
+        3: {"name": "Daniela", "age": "23", "nationality": "mexican"}
     }
 ]
 
-for n in data:
-    #print(n) # n is data[0] in the first for loop
-    print(n[1]["nationality"])
+for person in data:
+    for key, value in person.items():
+        print(value["nationality"])
 
+users = [
+    {
+        "name": "Daniela",
+        "age": 23,
+        "height": 157,
+        "nicknames": {
+            1: "Naan",
+            2: "Lovebug",
+            3: "Dani"
+        }
+    },
+    {
+        "name": "Cody",
+        "age": 26,
+        "height": 170,
+        "nicknames": {
+            1: "Lovebug",
+            2: "Codyman",
+            3: "Hawaii"
+        }
+    }
+]
 
+# for user in users:
+#     print(user["nicknames"].items())
+#     for key, value in user["nicknames"].items():
+#         print(key, value)
+
+# print(users[1])
